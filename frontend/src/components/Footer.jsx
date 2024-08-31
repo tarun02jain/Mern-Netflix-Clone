751479d81ee18e7f409db029c69c6a046daa8f65
Tarun Jain <144423619+tarun02jain@users.noreply.github.com>
2024-08-31 18:59:04 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ const Footer = () => {
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Built by{"Tarun Jain"}
+					Built by{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/tarun02jain'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						Tarun Jain
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/tarun02jain/Mern-Netflix-Clone'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
